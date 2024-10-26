c59805566d6de6407936784abf0ee5f4f4070e81
Monique Grinstein <monique.grinstein@lego.com>
2024-10-26 16:21:52 +0100

    refactor: move functionality to dedicated helper

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-from skyfield.api import load
-from datetime import datetime
-from config import SATELLITE_URL, SATELLITE_NAME
+from config import SATELLITE_URL, SATELLITE_NAME, OUTPUT_PATH
 from load_satellite_data import load_satellite_data
 from plot_satellite_position import plot_satellite_position
 
 def main():
-    satellite = load_satellite_data(SATELLITE_URL, SATELLITE_NAME)
-    # Create timescale object and get current time
-    ts = load.timescale()
-    t = ts.now()
-
-    # Get satellite position at the current time
-    geocentric = satellite.at(t)
-    subpoint = geocentric.subpoint()
-    print('Latitude:', subpoint.latitude)
-    print('Longitude:', subpoint.longitude)
-    print('Altitude (km):', subpoint.elevation.km)
-
-    plot_satellite_position(satellite.name, geocentric)
+    satellite_data = load_satellite_data(SATELLITE_URL, SATELLITE_NAME)
+    plot_satellite_position(satellite_data, OUTPUT_PATH)
 
 if __name__ == "__main__":
     main()
--- plot_satellite_position.py
+++ plot_satellite_position.py
@@ -1,14 +1,21 @@
 import folium
+from skyfield.api import load
 
 
-def plot_satellite_position(satellite_name, geocentric, output_path="satellite_position.html"):
+def plot_satellite_position(satellite_data, output_path):
+    # Create timescale object and get current time
+    ts = load.timescale()
+    time_now = ts.now()
+
+    # Get satellite position at the current time
+    geocentric = satellite_data.at(time_now)
     subpoint = geocentric.subpoint()
     latitude = subpoint.latitude.degrees
     longitude = subpoint.longitude.degrees
     altitude = subpoint.elevation.km
     m = folium.Map(location=[latitude, longitude], zoom_start=4)
 
-    tooltip = f'{satellite_name} (Altitude: {altitude:.2f} km)'
+    tooltip = f'{satellite_data.name} (Altitude: {altitude:.2f} km)'
     folium.Marker([latitude, longitude], popup=tooltip).add_to(m)
 
     save_and_display_map(m, output_path)
