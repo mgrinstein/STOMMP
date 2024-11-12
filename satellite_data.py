cd7cea16161d5d21dbb00199ed6999c37d17af50
Monique Calmon Ferreira Grinstein <moniquecalmon@poli.ufrj.br>
2024-11-12 18:28:38 -0300

    refactor: decouple parsing logic

--- satellite_data.py
+++ satellite_data.py
@@ -12,35 +12,37 @@ def fetch_tle_data(url):
         tle_data = response.text.strip().split('\n')
         if len(tle_data) % 3 != 0:
             raise ValueError(f"TLE data fetched from {url} is incomplete.")
-        
-        satellites = []
-        for i in range(0, len(tle_data), 3):
-            satellite_name = tle_data[i].strip()
-            line1 = tle_data[i + 1].strip()
-            line2 = tle_data[i + 2].strip()
-            satellite = EarthSatellite(line1, line2, satellite_name)
-            satellites.append(satellite)
-        
-        print(f"Successfully fetched {len(satellites)} satellites.")
-        return satellites
+        return tle_data
     except requests.RequestException as e:
         print(f"Failed to fetch TLE data from {url}: {e}")
-        return []
+        return ''
     except ValueError as e:
         print(e)
-        return []
+        return ''
 
+def parse_satellite_data(tle_data):
+    satellites = []
+    for i in range(0, len(tle_data), 3):
+        satellite_name = tle_data[i].strip()
+        line1 = tle_data[i + 1].strip()
+        line2 = tle_data[i + 2].strip()
+        satellite = EarthSatellite(line1, line2, satellite_name)
+        satellites.append(satellite)
+    return satellites
 
-def load_satellite_data(satellite_sources, desired_satellite_name): 
+def load_satellite_data(satellite_sources, desired_satellite): 
     selected_satellite = None
     
     for category_name, category_url in satellite_sources.items():
         try:
             satellites_in_category = fetch_tle_data(category_url)
-            for satellite in satellites_in_category:
+            parsed_satellites = parse_satellite_data(satellites_in_category)
+            print(f"Successfully fetched {len(parsed_satellites)} satellites.")
+            
+            for satellite in parsed_satellites:
 
-                if satellite.name == desired_satellite_name:
-                    print(f"Found satellite {desired_satellite_name} in source: {category_name} ({category_url})")
+                if satellite.name == desired_satellite:
+                    print(f"Found satellite {desired_satellite} in source: {category_name} ({category_url})")
                     selected_satellite = satellite
                     break
             if selected_satellite:
@@ -52,5 +54,5 @@ def load_satellite_data(satellite_sources, desired_satellite_name):
     if selected_satellite:
         return selected_satellite
     else:
-        print(f"Satellite {desired_satellite_name} not found in any of the provided sources.")
+        print(f"Satellite {desired_satellite} not found in any of the provided sources.")
         return None
